bc45d8dfca9cc066d79077b4345cb17f036cec23
Ruslan <ruslan.rusu@yahoo.com>
2019-10-06 16:58:09 -0400

    added versioning

--- scripts/rebuild-continer.sh
+++ scripts/rebuild-continer.sh
@@ -1,1 +1,12 @@
 docker build -t pdqtogo -f continer/Dockerfile .
+
+sh scripts/version.sh $(cat version.txt) feature > version.txt
+
+imageid=$(docker images | grep "pdqtogo" | grep "latest" | awk '{print $3}')
+version=$(cat version.txt)
+
+docker tag $imageid i95north/pdqtogo:$version
+
+echo "latest [pdqtogo#$imageid] was tagged with $version"
+
+docker push i95north/pdqtogo
